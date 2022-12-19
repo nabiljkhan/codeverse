26b6655ef759e8b6afb96a0a6f3c2777f01c45d5
Nabil Jahan Khan <83438134+nabiljkhan@users.noreply.github.com>
2022-12-19 18:53:51 +0530

    Update useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-const PREFIX = 'codepen-clone-'
+const PREFIX = 'codeverse'
 
 export default function useLocalStorage(key, initialValue) {
   const prefixedKey = PREFIX + key
